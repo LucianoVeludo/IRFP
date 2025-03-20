cd0fb3d562468449bab86366a0ca8a6994dc14e4
Luciano <103335090+LucianoVeludo@users.noreply.github.com>
2025-03-20 13:32:26 +0000

    Corrigindo App.js para exibir a Calculadora de IR

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import CalculadoraIR from "./Calculadora_IR_2025-2026"; // Importando o componente corretamente
 
 function App() {
-  return <h1>Olá, mundo! Meu React está funcionando!</h1>;
+  return (
+    <div>
+      <CalculadoraIR />
+    </div>
+  );
 }
 
 export default App;
