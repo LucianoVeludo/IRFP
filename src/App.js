241d323b02cbb2d263dc0e5e6c9730c798102ad8
Luciano <103335090+LucianoVeludo@users.noreply.github.com>
2025-03-20 19:42:07 +0000

    AdSense

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from "react";
-import CalculadoraIR from "./Calculadora_IR_2025-2026"; // Importando o componente corretamente
+import React from 'react';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import CalculadoraIR from './Calculadora_IR_2025-2026';
 
-function App() {
+const App = () => {
   return (
-    <div>
+    <HelmetProvider>
+      <Helmet>
+        {/* Código de verificação do Google AdSense */}
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-431303169883797"
+          crossOrigin="anonymous"></script>
+      </Helmet>
       <CalculadoraIR />
-    </div>
+    </HelmetProvider>
   );
-}
+};
 
 export default App;
